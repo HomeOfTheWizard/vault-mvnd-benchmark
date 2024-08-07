b789f9ca42c7721c883047ef94f2db191946894e
Ozgun OZ <ozgunn.oz@gmail.com>
2024-08-07 14:41:34 +0200

    refactor bash scripts

--- docker/launch-agent.sh
+++ docker/launch-agent.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
+echo "launching vault agent"
 docker compose -f vault/docker-compose-vault-agent.yaml up --build -d
--- docker/launch-vault.sh
+++ docker/launch-vault.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+echo "starting vault server container"
 docker compose -f vault/docker-compose-vault.yaml up --build -d
+
+read -r -p "Wait 5 seconds for the server to startup, or press any key to continue immediately" -t 5 -n 1 -s
+
+echo "configuring the vault server and setup up some secrets in kv engine"
 docker exec -d vault ./vault/configure-vault.sh
