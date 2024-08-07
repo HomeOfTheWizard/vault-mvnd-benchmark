c198dfd7ed2a59a01d077be1023f9b6551dda70b
Ozgun OZ <ozgunn.oz@gmail.com>
2024-08-07 14:57:55 +0200

    refactor bash scripts

--- docker/launch-vault.sh
+++ docker/launch-vault.sh
@@ -5,5 +5,6 @@ docker compose -f vault/docker-compose-vault.yaml up --build -d
 
 read -r -p "Wait 5 seconds for the server to startup, or press any key to continue immediately" -t 5 -n 1 -s
 
+echo ""
 echo "configuring the vault server and setup up some secrets in kv engine"
 docker exec -d vault ./vault/configure-vault.sh
