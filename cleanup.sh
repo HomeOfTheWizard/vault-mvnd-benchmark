46dba14ab57ef0168159001987dc1aa45cb94c3f
Ozgun OZ <ozgunn.oz@gmail.com>
2024-08-08 19:15:28 +0200

    cleanup volumes from mvnd and agent containers

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,10 @@
 # stop vault and cAdvisor after test reports are created
-docker compose -f vault/docker-compose-vault.yaml down -v
 docker compose -f agent/docker-compose-vault-agent.yaml down -v
+docker container stop mnvd
+docker container rm mnvd
+docker compose -f vault/docker-compose-vault.yaml down -v
 docker compose -f monitoring/docker-compose-monitoring.yaml down -v
+docker volume rm $(docker volume ls -q -f dangling=true)
 
 # cleanup files
 sudo rm docker/agent/secrets/application-agent.yaml
