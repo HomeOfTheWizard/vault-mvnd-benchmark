fa7426dd177bb7059551fe4512992a4047422fee
Ozgun OZ <ozgunn.oz@gmail.com>
2024-08-09 21:11:31 +0200

    update docs

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,9 @@
 # stop vault and cAdvisor after test reports are created
-docker compose -f agent/docker-compose-vault-agent.yaml down -v
-docker container stop mvnd
-docker container rm mvnd
-docker compose -f vault/docker-compose-vault.yaml down -v
-docker compose -f monitoring/docker-compose-monitoring.yaml down -v
+docker compose -f docker/agent/docker-compose-vault-agent.yaml down -v
+docker container stop mvnd-vault
+docker container rm mvnd-vault
+docker compose -f docker/vault/docker-compose-vault.yaml down -v
+docker compose -f docker/monitoring/docker-compose-monitoring.yaml down -v
 docker volume rm $(docker volume ls -q -f dangling=true)
 
 # cleanup files
